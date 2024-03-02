84219a582ba391cb65d473adbfe5ba42e791ed05
Jing Yeh <yhxjin001@myuct.ac.za>
2024-03-02 18:57:30 +0200

    Created the KnowledgeBase interface

---
+++ Assignment1/TheCommonSenseLibrary/src/main/KnowledgeBase.java
@@ -0,0 +1,31 @@
+import java.io.FileReader;
+public interface KnowledgeBase {
+    /**
+     * To add all the statements in a file being read by a Reader to the KnowledgeBase.
+     * Note that statements with the same statement would be updated based on the confidence score,
+     * with the one with lower confidence score discarded.
+     * @param FileReader inputFileReader: The name of the reader extracting the data from the file;
+     */
+    public void add(FileReader inputFileReader);
+
+    /**
+     * To add a single statement to the KnowledgeBase
+     * Note that statements with the same statement would be updated based on the confidence score,
+     * with the one with lower confidence score discarded.
+     * @param String statement: The statement to be written to the KnowledgeBase
+     */
+    public void add(String statement);
+
+    /** 
+     * To write the loaded knowledge base into a file
+     * @param String outputFile: The name of the file you'd like to write into
+     */
+    public void save(String outputFile);
+
+    /**
+     * To return the Entry that stores the given term
+     * @param String term: The term of the statement you'd like to search for
+     */
+    public Entry search(String term);
+
+}
